d4ee78c1872893c540240ce60b3c652e1c569d95
Patrick Logan <tennix@msn.com>
2022-12-27 15:12:04 -0800

    Ownership stuff

--- guessing_game/target/debug/deps/guessing_game-fab5eb9fe9d36647.d
+++
@@ -1,5 +0,0 @@
-/Users/plogan/code/rustlearn/guessing_game/target/debug/deps/guessing_game-fab5eb9fe9d36647: src/main.rs
-
-/Users/plogan/code/rustlearn/guessing_game/target/debug/deps/guessing_game-fab5eb9fe9d36647.d: src/main.rs
-
-src/main.rs:
